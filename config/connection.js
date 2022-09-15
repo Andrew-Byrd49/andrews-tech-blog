e06eee9fdc57709b423f13194e526ac71d4c0502
Andrew Byrd <andrew.byrds2@gmail.com>
2022-09-15 12:20:25 -0400

    trying to fix heroku application error

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,7 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-// Connect to database
-
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
@@ -11,9 +10,9 @@ const sequelize = process.env.JAWSDB_URL
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
-        host: "localhost",
+        host: "127.0.0.1",
         dialect: "mysql",
-        port: 3306
+        port: 3306,
       }
     );
 
